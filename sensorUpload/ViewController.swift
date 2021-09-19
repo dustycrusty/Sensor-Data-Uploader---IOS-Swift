99c0d3afc8c755d0f97444d91d107a569da041e5
Dustin <sylee970711@gmail.com>
2021-09-19 14:59:19 -0400

    Initial Commit

---
+++ sensorUpload/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sensorUpload
+//
+//  Created by 이승윤 on 2021/09/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
